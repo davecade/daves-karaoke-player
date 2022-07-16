f7381c81d1c85c0136698848193481eed833c6f6
Dave <dave.cadelina@outlook.com>
2022-07-16 23:13:19 +1000

    created basic function

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
 import "./App.css";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactPlayer from "react-player";
 
 function App() {
+    const [selectedFiles, setSelectedFiles] = useState([]);
+    const [playing, setPlaying] = useState(false);
+    const [videos, setVideos] = useState([]);
+    const [videoToPlay, setVideoToPlay] = useState(0);
+
+    useEffect(() => {
+        if (selectedFiles?.length > 0) {
+            const convertedVideos = [];
+            for (let i = 0; i < selectedFiles.length; i++) {
+                convertedVideos.push(URL.createObjectURL(selectedFiles[i]));
+            }
+            setVideos(convertedVideos);
+            setPlaying(true);
+        } else {
+            setPlaying(false);
+        }
+    }, [selectedFiles]);
+
     return (
         <div className="App">
             <ReactPlayer
                 controls
-                playing
-                url="https://www.youtube.com/watch?v=rjPg9JqTxkY"
+                playing={playing}
+                url={videos[videoToPlay]}
+                onEnded={() => setVideoToPlay(videoToPlay + 1)}
+            />
+            <input
+                type="file"
+                multiple
+                onChange={(e) => {
+                    setSelectedFiles(e.target.files);
+                }}
             />
         </div>
     );
